62483f95148ee38404afd25afafa8d2fc0b91280
arigkubra <arigkubra@gmail.com>
2023-10-15 23:47:13 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  
+  <header>
+    <div class="container">
+      <img src="@/assets/pinia-logo.svg" alt="Pinia" title="Pinia">
+      <h1>Kişisel Günlüğüm</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
